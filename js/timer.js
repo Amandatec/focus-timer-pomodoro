c5f5db6a6a02496b5c6694cdbe11a6dfe7ede5a3
Amandatec <amanda.oliv@hotmail.com>
2023-03-05 07:40:54 -0300

    add sound to the app

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,10 @@
+import Sounds from "./sounds.js"
+
 export default function Timer({ 
   minutesDisplay, 
   secondsDisplay, 
-  resetControls
+  resetControls,
+  sound
 }) {
 
   let timerTimeOut
@@ -31,6 +34,7 @@ export default function Timer({
       if (isFinished) {
         resetControls()
         updateDisplay()
+        Sounds().timeEnd()
         return
       }
 
